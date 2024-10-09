f167af0f450d67b54e512fae1709fdeffb5b57fd
JoseOtero15 <joseotero1504@gmail.com>
2024-10-09 14:06:15 -0500

    Markers added over the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,17 +35,31 @@ export class AppComponent implements OnInit {
           center: this.currentPosition, // Centrar el mapa en la ubicación actual
           zoom: 14 // Nivel de zoom
         });
-
+        console.log(this.currentPosition)
         // Agregar un marcador en la ubicación actual
-        new mapboxgl.Marker()
-          .setLngLat(this.currentPosition)
-          .addTo(this.map);
+        this.map.on('load', () => {
+          this.addMarker(this.currentPosition, 'Marker 1'); // Marcador 1
+          this.addMarker([-76.49681942762918,3.478120048244367], 'Marcador 2 de prueba, aca pondría información del sitio'); // Marcador 1
+        });
       }, (error) => {
         console.error('Error al obtener la geolocalización', error);
       });
     } else {
       console.error('Geolocalización no es soportada por este navegador.');
     }
+  }
+
+  addMarker(coordinates: [number, number], title: string): void {
+    /* const markerElement = document.createElement('div');
+    markerElement.className = 'my-marker'; // Clase CSS personalizada
+    markerElement.style.backgroundImage = 'url(path_to_your_icon.png)';
+    markerElement.style.width = '30px'; // Ancho del ícono
+    markerElement.style.height = '30px'; // Alto del ícono */
+    // Crear un nuevo marcador
+    const marker = new mapboxgl.Marker()
+      .setLngLat(coordinates) // Establecer la ubicación del marcador
+      .setPopup(new mapboxgl.Popup().setText(title)) // Agregar un popup con el título
+      .addTo(this.map); // Añadir el marcador al mapa
   }
 
   // Método para cargar Google Maps
