249ea1838eb5f1fe229099b030e8f71635eca818
JoseOtero15 <joseotero1504@gmail.com>
2024-10-09 13:31:02 -0500

    google maps config has been added but was commented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +8,25 @@ import { environment } from '../enviroments/enviroment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // googleCenter: google.maps.LatLngLiteral = { lat: 40.730610, lng: -73.935242 }; // Coordenadas iniciales de Google Maps
-
+  // googleCenter: google.maps.LatLngLiteral = { lat: 40.730610, lng: -73.935242 };
   map!: mapboxgl.Map; // Declaramos el mapa como propiedad
   currentPosition: [number, number] = [0, 0]; // Para almacenar la posición actual
 
   ngOnInit(): void {
+    // this.loadGoogleMaps(); // Cargar Google Maps API
+
     // Intentar obtener la ubicación del usuario
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         // Obtener latitud y longitud del usuario
         this.currentPosition = [position.coords.longitude, position.coords.latitude];
         
+        // Actualizar el centro de Google Maps con la ubicación actual
+        /* this.googleCenter = { 
+          lat: position.coords.latitude, 
+          lng: position.coords.longitude 
+        }; */
+
         // Inicializar el mapa de Mapbox en la ubicación del usuario
         this.map = new mapboxgl.Map({
           accessToken: environment.mapboxAccessToken, // Token de Mapbox
@@ -40,4 +47,13 @@ export class AppComponent implements OnInit {
       console.error('Geolocalización no es soportada por este navegador.');
     }
   }
+
+  // Método para cargar Google Maps
+ /*  loadGoogleMaps() {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+  } */
 }
